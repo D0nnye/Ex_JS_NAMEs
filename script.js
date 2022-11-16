8105c8126c051351932a2283a5e70cf5ddf85add
D0nnye <lucas.sleocadio@senacsp.edu.br>
2022-11-16 16:08:02 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function clicou(){
+    document.querySelector("#whatever").innerText = "clicando"
+}
+function dentro(){
+    document.querySelector("#whatever").innerText = "estou dentro"
+}
+function fora(){
+    document.querySelector("#whatever").innerText = "estou fora"
+}
